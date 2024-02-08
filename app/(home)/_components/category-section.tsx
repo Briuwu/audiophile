a60542dce465c92d27986a2425b162efdb77ed0b
Briuwu <millontebrian@gmail.com>
2024-02-08 23:26:07 +0800

    Update category section styles and links

--- app/(home)/_components/category-section.tsx
+++ app/(home)/_components/category-section.tsx
@@ -6,28 +6,33 @@ import Link from "next/link";
 
 export function CategorySection() {
   return (
-    <ul className="mt-16 space-y-16 md:grid md:grid-cols-3 md:gap-5 md:space-y-0">
+    <ul className="my-16 space-y-16 md:my-32 md:grid md:grid-cols-3 md:gap-5 md:space-y-0">
       {navitem.map((item) => (
         <li
           key={item.title}
-          className="relative flex flex-col items-center rounded-md bg-gray-400 p-5 pt-12"
+          className="group relative items-center rounded-md bg-gray-400"
         >
-          <div className="flex flex-col items-center space-y-4">
+          <Link
+            href={item.href}
+            className="flex h-full w-full flex-col items-center space-y-4 p-5 pt-12 lg:p-8 lg:pt-32"
+          >
             <Image
-              src={item.image}
+              src={`/assets/shared/desktop/${item.image}`}
               alt={item.title}
-              width={80}
-              height={104}
-              className={cn("absolute", item.position)}
+              width={item.width}
+              height={item.height}
+              className={cn("absolute w-36 lg:w-60", item.position)}
             />
-            <p className="text-sm font-semibold uppercase">{item.title}</p>
-            <Link href={item.href} className="flex items-center">
-              <span className="text-sm font-semibold uppercase opacity-50">
+            <p className="text-sm font-semibold uppercase lg:text-lg">
+              {item.title}
+            </p>
+            <p className="flex items-center">
+              <span className="text-sm font-semibold uppercase opacity-50 group-hover:text-orange-800">
                 shop
               </span>
               <ChevronRight className="ml-2 h-4 w-4 text-orange-800" />
-            </Link>
-          </div>
+            </p>
+          </Link>
         </li>
       ))}
     </ul>
